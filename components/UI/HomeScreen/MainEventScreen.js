1f6f90c8f5683591776656b42fb954321a700b0d
kukli07 <timemir97@gmail.com>
2022-08-18 15:17:55 +0200

    Add Calendar Entry
    - Installed Expo Calendar
    
    - Implemented a lot of logic to use Expo Calendar inside the Main Event Screen

--- components/UI/HomeScreen/MainEventScreen.js
+++ components/UI/HomeScreen/MainEventScreen.js
@@ -1,3 +1,4 @@
+import * as Calendar from "expo-calendar";
 import React, { useEffect, useState } from "react";
 import { Pressable, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import { showLocation } from "react-native-map-link";
@@ -10,6 +11,7 @@ import {
   Colors,
   ExpandableSection,
   Image,
+  Incubator,
   Spacings,
   Text,
   View,
@@ -17,10 +19,14 @@ import {
 import { auth } from "../../../store/firebase";
 import { updateParticipants } from "../../../store/http";
 import { categoryImage } from "../../../util/categoriesImages";
+const { Toast } = Incubator;
+// Async Storage to store first time user created
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { create } from "yup/lib/array";
 // Component
 export default function MainEventScreen(props) {
   const eventObject = props.route.params;
-
+  // console.log(eventObject);
   // Date Management
   const date = new Date(eventObject.date);
   const longDate = date.toLocaleDateString("de-DE").split("-");
@@ -74,6 +80,142 @@ export default function MainEventScreen(props) {
     props.navigation.goBack();
     props.navigation.navigate("Map");
     // TODO: Then highlight the pin of the given Event
+  }
+
+  // Calendar Handling
+  const [showCalendarModal, setShowCalendarModal] = useState(false);
+  const [showEventAlreadyCreatedModal, setShowEventAlreadyCreatedModal] =
+    useState(false);
+  async function getDefaultCalendarSource() {
+    const defaultCalendar = await Calendar.getDefaultCalendarAsync();
+    return defaultCalendar.source;
+  }
+
+  const saveCalendarID = async (id) => {
+    try {
+      await AsyncStorage.setItem("@eventify_calendar", id);
+      console.log("Set AsyncStorage successfully");
+      console.log(id);
+    } catch (error) {
+      // saving error
+      console.log(error);
+    }
+  };
+  async function getCalendarID() {
+    try {
+      const value = await AsyncStorage.getItem("@eventify_calendar");
+      return value;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function createCalendar() {
+    const defaultCalendarSource =
+      Platform.OS === "ios"
+        ? await getDefaultCalendarSource()
+        : { isLocalAccount: true, name: "Eventify" };
+    const newCalendarID = await Calendar.createCalendarAsync({
+      title: "Eventify",
+      color: "orange",
+      entityType: Calendar.EntityTypes.EVENT,
+      sourceId: defaultCalendarSource.id,
+      source: defaultCalendarSource,
+      name: "internalCalendarName",
+      ownerAccount: "personal",
+      accessLevel: Calendar.CalendarAccessLevel.OWNER,
+    });
+    // console.log(`Your new calendar ID is: ${newCalendarID}`);
+    saveCalendarID(newCalendarID);
+  }
+
+  function calendarHandler() {
+    (async () => {
+      const { status } = await Calendar.requestCalendarPermissionsAsync();
+      if (status === "granted") {
+        const calendars = await Calendar.getCalendarsAsync(
+          Calendar.EntityTypes.EVENT
+        );
+
+        const eventifyCalendarId = await getCalendarID();
+        let disableCalendarCreation = false;
+        for (const index in { calendars }.calendars) {
+          if ({ calendars }.calendars[index].id === eventifyCalendarId) {
+            disableCalendarCreation = true;
+          }
+        }
+        if (disableCalendarCreation === false) {
+          createCalendar();
+        }
+        // console.log("Calendar already created");
+        // console.log(
+        //   { calendars }.calendars.filter(
+        //     (calendar) => calendar.id === eventifyCalendarId
+        //   )
+        // );
+
+        const calendarEventDetails = {
+          calendarId: eventifyCalendarId,
+          id: eventObject.eventId,
+          location: `${eventObject.location.name}
+          ${eventObject.location.street}, ${eventObject.location.city}`,
+          organizer: eventObject.createdBy.userName,
+          notes: eventObject.description,
+          startDate: `${eventObject.date}T${eventObject.time.slice(
+            0,
+            5
+          )}:00.000Z`,
+          endDate: `${eventObject.date}T${
+            parseInt(eventObject.time.slice(0, 2)) + 2
+          }${eventObject.time.slice(2, 5)}:00.000Z`,
+          title: eventObject.title,
+          status: Calendar.EventStatus.CONFIRMED,
+          timeZone: "UTC+01:00",
+          allDay: false,
+
+          availability: Calendar.Availability.BUSY,
+        };
+        async function createEvent() {
+          try {
+            const createdEvent = await Calendar.createEventAsync(
+              eventifyCalendarId,
+              calendarEventDetails
+            );
+            console.log("created Event");
+            return createdEvent;
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        async function getCalendarEvents() {
+          try {
+            const test = await Calendar.getEventsAsync(
+              [eventifyCalendarId],
+              new Date("2022-01-01"),
+              new Date("2022-12-12")
+            );
+            return test;
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        const currentEvents = await getCalendarEvents();
+
+        // Check if the Event already exists
+        let eventAlreadyExists = false;
+        currentEvents.forEach((event) => {
+          if (event.title === eventObject.title) {
+            eventAlreadyExists = true;
+          }
+        });
+
+        if (!eventAlreadyExists) {
+          createEvent();
+          setShowCalendarModal(true);
+        } else {
+          setShowEventAlreadyCreatedModal(true);
+        }
+      }
+    })();
   }
   return (
     <SafeAreaView style={styles.container}>
@@ -153,7 +295,7 @@ export default function MainEventScreen(props) {
                     backgroundColor={Colors.secondaryColor}
                     avoidMinWidth
                     style={{ width: 50, height: 50 }}
-                    onPress={() => console.log("Opening calendar")}
+                    onPress={calendarHandler}
                   ></Button>
                 </View>
               </Card>
@@ -250,6 +392,24 @@ export default function MainEventScreen(props) {
             </View>
           </View>
         </View>
+        <Toast
+          visible={showCalendarModal}
+          centerMessage
+          message="Kalendereintrag erstellt"
+          preset="success"
+          position={"top"}
+          autoDismiss={5000}
+          onDismiss={() => setShowCalendarModal(false)}
+        ></Toast>
+        <Toast
+          centerMessage
+          visible={showEventAlreadyCreatedModal}
+          message="Kalendereintrag existiert bereits"
+          preset="failure"
+          position={"top"}
+          autoDismiss={5000}
+          onDismiss={() => setShowEventAlreadyCreatedModal(false)}
+        ></Toast>
       </ScrollView>
       <View row>
         <View row flex marginT-15 marginL-20>
@@ -295,6 +455,7 @@ export default function MainEventScreen(props) {
             />
           ) : null}
         </View>
+
         <View flex marginT-10 height={60}>
           {userIsParticipant ? (
             <Button
